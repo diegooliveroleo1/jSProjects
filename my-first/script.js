794098a225ea35005354fb2f1071041ca70ca82b
Diego Olivero <diego@email.com>
2023-07-02 14:46:10 +0200

    Add panels

---
+++ my-first/script.js
@@ -0,0 +1,19 @@
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        if (panel.classList.contains('active')) {
+            removeActive();
+        } else {
+            removeActive();
+            panel.classList.add('active');    
+        }
+    })
+});
+
+function removeActive() {
+    panels.forEach(panel => {
+        panel.classList.remove('active');
+    })
+};
+
